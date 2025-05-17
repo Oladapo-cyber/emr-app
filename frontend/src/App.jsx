bdcb3a685b1b80bca65102afd3a3b9284619ce6b
daps <ydapo50@gmail.com>
2025-05-17 13:00:45 +0100

    feat: restructure App component with routing and new layout
    - Integrated Navbar and Sidebar components for navigation
    - Added routing for Home, Patients, New Patient, Medical Records, and Scheduling pages
    - Included Footer component for consistent layout
    - Updated package.json to include new dependencies for styling and icons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Home from "./pages/Home";
+import MedicalRecords from "./pages/MedicalRecords";
+import NewPatient from "./pages/NewPatient";
+import Patients from "./pages/Patients";
+import Scheduling from "./pages/Scheduling";
+import Footer from "./components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Navbar />
+      <div className="flex">
+        <div className="bg-gray-100">
+          <Sidebar />
+        </div>
+
+        <div className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/patients" element={<Patients />} />
+            <Route path="/new-patient" element={<NewPatient />} />
+            <Route path="/medical-records" element={<MedicalRecords />} />
+            <Route path="/scheduling" element={<Scheduling />} />
+          </Routes>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
