a60d2bdc50fc247ab0fe9c683f88319720d1ddb6
daps <ydapo50@gmail.com>
2025-05-28 23:49:34 +0100

    feat: implement PatientDetails component with avatar and edit functionality

--- frontend/src/components/PatientDetails.jsx
+++ frontend/src/components/PatientDetails.jsx
@@ -1,7 +1,21 @@
+import { Avatar } from "@mui/material";
+
+import { StickyNote2Outlined } from "@mui/icons-material";
 const PatientDetails = () => {
   return (
     <div>
-      <div></div>
+      <div className="flex">
+        <Avatar sx={{ width: 50, height: 50 }}>YO</Avatar>
+        <div className="flex flex-col items-center justify-center ml-4">
+          <p>Yinusa Oladapo</p>
+          <div className="flex items-center space-x-2 border">
+            <StickyNote2Outlined className="text-blue-500" />
+            <button className="bg-transparent border-none text-blue-600 hover:font-medium hover:text-blue-700 p-0 m-0 focus:outline-none">
+              Edit
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
